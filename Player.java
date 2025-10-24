23c8779c1be6e165ad1aadadbef8a6167e35df1a
lbaker13 <lucas.baker136@gmail.com>
2025-10-24 11:30:35 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+
+public class Player
+{
+    private String name;
+    private ArrayList<Card> hand;
+    private int score;
+    private int numCards;
+
+    /**
+     * Constructs a new player with an empty card list and a score of 0.
+     */
+    public Player()
+    {
+        hand = new ArrayList<Card>();
+        numCards = 0;
+        score = 0;
+    }
+    
+    /**
+     * Gets list of cards in the player's hand.
+     * @return ArrayList containing the player's cards.
+     */
+    public ArrayList<Card> getHand()
+    {
+        return hand;
+    }
+    
+    /**
+     * Gets number of cards in the player's hand.
+     * @return number of cards in the player's hand.
+     */
+    public int getNumCards()
+    {
+        return numCards;
+    }
+
+    /**
+     * Gets the player's score.
+     * @return the player's score.
+     */
+    public int getScore()
+    {
+        return score;
+    }
+
+    /**
+     * Gets name of the player.
+     * @return name of the player.
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Sets the player's score.
+     * @param score
+     * The score to set for the player.
+     */
+    public void setScore(int score)
+    {
+        this.score = score;
+    }
+
+    /**
+     * Sets name of the player.
+     * @param name
+     * The name to set for the player.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Adds a card to the player's hand, increases card count.
+     * @param card
+     * The card to add to the player's hand.
+     */
+    public void addCard(Card card)
+    {
+        hand.add(card);
+        numCards++;
+    }
+
+    /*
+     * Draws card from the deck, adds it to the player's hand, increases card count.
+     * @param deck
+     * The Uno deck to draw a card.
+     */
+    public void drawCard(Deck deck)
+    {
+        hand.add(deck.draw());
+        numCards++;
+    }
+
+    /*
+     * Removes card from the player's hand by its index, decreases card count.
+     * @param index
+     * The index of the card in the list to remove from the player's hand.
+     */
+    public void removeCard(int index)
+    {
+        hand.remove(index);
+        numCards--;
+    }
+
+    /*
+     * Plays card from the player's hand.
+     * @param index
+     * The index of the card to play in the player's hand.
+     * @return the card played.
+     */
+    public Card playCard(int index)
+    {
+        return hand.get(index);
+    }
+
+    /*
+     * Displays the player's cards.
+     */
+    public void displayCards()
+    {
+        System.out.println("Your Cards:");
+        for (int i = 0; i < hand.size(); i++)
+        {
+            System.out.println(i + 1 + "." + hand.get(i).toString());
+        }
+    }
+}
---
+++ PlayerTest.java
@@ -0,0 +1,177 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+public class PlayerTest
+{
+    private static Player player;
+    private static int count;
+
+    public PlayerTest() {}
+
+    /*
+     * Sets test player and counter to default values before each test
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        player = null;
+    }
+
+    /*
+     * Displays the counter value for each test after they are complete
+     */
+    @AfterEach
+    public void summary() 
+    {
+        System.out.println("Number of Tests Completed: " + count + "\n");
+    }
+
+    @Test
+    public void test_DefaultConstructor() 
+    {
+        System.out.println("Testing the default constructor");
+        ArrayList<Card> temp_list = new ArrayList<>();
+        player = new Player();
+        assertEquals(0, player.getNumCards());
+        assertEquals(temp_list, player.getHand());
+        count = 2;
+    }
+    
+    @Test
+    public void test_getHand() 
+    {
+        System.out.println("Testing the getHand() method");
+        ArrayList<Card> temp_list = new ArrayList<>();
+        temp_list.add(new Card(Card.CardType.THREE, Card.Colour.RED));
+        temp_list.add(new Card(Card.CardType.REVERSE, Card.Colour.GREEN));
+        temp_list.add(new Card(Card.CardType.SKIP, Card.Colour.BLUE));
+
+        player = new Player();
+        player.addCard(new Card(Card.CardType.THREE, Card.Colour.RED));
+        player.addCard(new Card(Card.CardType.REVERSE, Card.Colour.GREEN));
+        player.addCard(new Card(Card.CardType.SKIP, Card.Colour.BLUE));
+        for (int i = 0; i < temp_list.size(); i++) {
+            assertEquals(temp_list.get(i).getCardType(), player.getHand().get(i).getCardType());
+            assertEquals(temp_list.get(i).getColour(), player.getHand().get(i).getColour());
+            count += 2;
+        }
+    }
+    
+    @Test
+    public void test_getNumCards() 
+    {
+        System.out.println("Testing the getNumCards() method");
+        player = new Player();
+        player.addCard(new Card(Card.CardType.EIGHT, Card.Colour.YELLOW));
+        player.addCard(new Card(Card.CardType.ONE, Card.Colour.RED));
+        assertEquals(2, player.getNumCards());
+        player.addCard(new Card(Card.CardType.WILD, Card.Colour.WILD));
+        assertEquals(3, player.getNumCards());
+        count = 2;
+    }
+
+    @Test
+    public void test_getScore() 
+    {
+        System.out.println("Testing the getScore() method");
+        player = new Player();
+        player.setScore(10);
+        assertEquals(10, player.getScore());
+        count = 1;
+    }
+
+    @Test
+    public void test_getName() 
+    {
+        System.out.println("Testing the getName() method");
+        player = new Player();
+        player.setName("Lucas");
+        assertEquals("Lucas", player.getName());
+        count = 1;
+    }
+
+    @Test
+    public void test_setScore() 
+    {
+        System.out.println("Testing the setScore() method");
+        player = new Player();
+        player.setScore(20);
+        assertEquals(20, player.getScore());
+        player.setScore(0);
+        assertEquals(0, player.getScore());
+        count = 2;
+    }
+
+    @Test
+    public void test_setName() 
+    {
+        System.out.println("Testing the setName() method");
+        player = new Player();
+        player.setName("Bobby");
+        assertEquals("Bobby", player.getName());
+        count = 1;
+    }
+
+    @Test
+    public void test_addCard() 
+    {
+        System.out.println("Testing the addCard() method");
+        player = new Player();
+        player.addCard(new Card(Card.CardType.FOUR, Card.Colour.YELLOW));
+        assertEquals(1, player.getNumCards());
+        assertEquals(Card.CardType.FOUR, player.getHand().get(0).getCardType());
+        assertEquals(Card.Colour.YELLOW, player.getHand().get(0).getColour());
+        count = 3;
+    }
+
+    @Test
+    public void test_drawCard() 
+    {
+        System.out.println("Testing the drawCard() method");
+        Deck deck = new Deck();
+        player = new Player();
+        player.drawCard(deck);
+        assertEquals(1, player.getNumCards());
+        assertEquals(103, deck.getNumDeckCards());
+        player.drawCard(deck);
+        assertEquals(2, player.getNumCards());
+        assertEquals(102, deck.getNumDeckCards());
+        count = 4;
+    }
+
+    @Test
+    public void test_removeCard() 
+    {
+        System.out.println("Testing the removeCard() method");
+        player = new Player();
+        player.addCard(new Card(Card.CardType.SEVEN, Card.Colour.RED));
+        player.addCard(new Card(Card.CardType.REVERSE, Card.Colour.BLUE));
+        player.addCard(new Card(Card.CardType.SKIP, Card.Colour.YELLOW));
+        player.addCard(new Card(Card.CardType.TWO, Card.Colour.RED));
+        player.removeCard(1);
+        assertEquals(3, player.getNumCards());
+        assertEquals(Card.CardType.SEVEN, player.getHand().get(0).getCardType());
+        assertEquals(Card.Colour.RED, player.getHand().get(0).getColour());
+        assertEquals(Card.CardType.SKIP, player.getHand().get(1).getCardType());
+        assertEquals(Card.Colour.YELLOW, player.getHand().get(1).getColour());
+        assertEquals(Card.CardType.TWO, player.getHand().get(2).getCardType());
+        assertEquals(Card.Colour.RED, player.getHand().get(2).getColour());
+        count = 7;
+    }
+
+    @Test
+    public void test_playCard() 
+    {
+        System.out.println("Testing the playCard() method");
+        player = new Player();
+        player.addCard(new Card(Card.CardType.FOUR, Card.Colour.BLUE));
+        player.addCard(new Card(Card.CardType.REVERSE, Card.Colour.BLUE));
+        Card played = player.playCard(1);
+        assertEquals(Card.CardType.REVERSE, played.getCardType());
+        assertEquals(Card.Colour.BLUE, played.getColour());
+        count = 2;
+    }
+}
